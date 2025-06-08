12bf801e0809f8be790646770591aa12b0d2b0df
saidi-22 <saidiasimaa@gmail.com>
2025-06-08 10:44:52 +0200

    settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "malvin~pidVQY4I#SgYjA189FFCng8XUxFmwZ5mnRdg7IeDYf54XzfPicdM",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
